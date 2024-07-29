ad13175d8207958f8d30730fbd68a76b4596a679
Kerem Mert <kerem37mert@yahoo.com>
2024-07-29 16:19:09 +0300

    variable created

---
+++ src/parser/variables/variable.c
@@ -0,0 +1,5 @@
+//
+// Created by Kerem on 29/07/2024.
+//
+
+#include "variable.h"
---
+++ src/parser/variables/variable.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 29/07/2024.
+//
+
+#ifndef VARIABLE_H
+#define VARIABLE_H
+
+#endif //VARIABLE_H
