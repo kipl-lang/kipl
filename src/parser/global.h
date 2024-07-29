296aa7018ea98ce2742c1ae946e7fbdc0a94c0bd
Kerem Mert <kerem37mert@yahoo.com>
2024-07-29 16:15:53 +0300

    scope created

---
+++ src/parser/global.h
@@ -0,0 +1,10 @@
+//
+// Created by Kerem on 29/07/2024.
+//
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+extern Token* currentToken;
+
+#endif //GLOBAL_H
---
+++ src/parser/scope/scope.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 29/07/2024.
+//
+
+#ifndef SCOPE_H
+#define SCOPE_H
+
+#endif //SCOPE_H
