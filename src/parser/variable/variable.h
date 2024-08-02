3f394d96b450deb4e8ae7bb2b37de8efc05cb743
Kerem Mert <kerem37mert@yahoo.com>
2024-08-02 11:07:56 +0300

    dataType added

--- src/parser/variable/variable.h
+++ src/parser/variable/variable.h
@@ -5,6 +5,21 @@
 #ifndef VARIABLE_H
 #define VARIABLE_H
 
-
+typedef enum _data_type {
+    TYPE_I8,
+    TYPE_I16,
+    TYPE_I32,
+    TYPE_I64,
+    TYPE_I128,
+    TYPE_U8,
+    TYPE_U16,
+    TYPE_U32,
+    TYPE_U64,
+    TYPE_U128,
+    TYPE_F32,
+    TYPE_F64,
+    TYPE_BOOL,
+    TYPE_STRING
+} DataType;
 
 #endif //VARIABLE_H
