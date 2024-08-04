6fa2848ebac7f42a3154f88d00c53f747cbce7a2
Kerem Mert <kerem37mert@yahoo.com>
2024-08-04 16:34:44 +0300

    dataTypes completed

--- src/parser/dataTypes.h
+++ src/parser/dataTypes.h
@@ -5,4 +5,21 @@
 #ifndef DATATYPES_H
 #define DATATYPES_H
 
+typedef enum _data_type {
+    TYPE_I8,
+    TYPE_I16,
+    TYPE_I32,
+    TYPE_I64,
+    TYPE_I128,
+    TYPE_U8,
+    TYPE_U16,
+    TYPE_U32,
+    TYPE_U64,
+    TYPE_U128,
+    TYPE_F32,
+    TYPE_F64,
+    TYPE_BOOL,
+    TYPE_STRING
+} DataType;
+
 #endif //DATATYPES_H
--- src/parser/variable/variable.h
+++ src/parser/variable/variable.h
@@ -15,25 +15,9 @@
 #ifndef VARIABLE_H
 #define VARIABLE_H
 
+#include "../dataTypes.h"
 #include <stdint.h>
 #include <stdbool.h>
-
-typedef enum _data_type {
-    TYPE_I8,
-    TYPE_I16,
-    TYPE_I32,
-    TYPE_I64,
-    TYPE_I128,
-    TYPE_U8,
-    TYPE_U16,
-    TYPE_U32,
-    TYPE_U64,
-    TYPE_U128,
-    TYPE_F32,
-    TYPE_F64,
-    TYPE_BOOL,
-    TYPE_STRING
-} DataType;
 
 typedef struct _i8_variable {
     char* name;
