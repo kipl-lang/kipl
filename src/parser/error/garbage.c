162cc234fc9ff20f64da158f9b1400efd461b271
Kerem Mert <kerem37mert@yahoo.com>
2024-08-06 08:56:01 +0300

    garbage.h added

---
+++ src/parser/error/garbage.c
@@ -0,0 +1,5 @@
+//
+// Created by Kerem on 06/08/2024.
+//
+
+#include "garbage.h"
---
+++ src/parser/error/garbage.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 06/08/2024.
+//
+
+#ifndef GARBAGE_H
+#define GARBAGE_H
+
+#endif //GARBAGE_H
