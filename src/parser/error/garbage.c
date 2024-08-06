5d565f75d69672ba566b89505881bb9f7996178a
Kerem Mert <kerem37mert@yahoo.com>
2024-08-06 11:19:55 +0300

    garbage removed

--- src/parser/error/garbage.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Kerem on 06/08/2024.
-//
-
-#include "garbage.h"
--- src/parser/error/garbage.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Kerem on 06/08/2024.
-//
-
-#ifndef GARBAGE_H
-#define GARBAGE_H
-
-#endif //GARBAGE_H
