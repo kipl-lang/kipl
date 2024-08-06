0eedf18be333430efbe23dae82b140582451ba5b
Kerem Mert <kerem37mert@yahoo.com>
2024-08-06 11:54:06 +0300

    stack and queue data created

---
+++ src/parser/expressions/queueData.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 06/08/2024.
+//
+
+#ifndef QUEUEDATA_H
+#define QUEUEDATA_H
+
+#endif //QUEUEDATA_H
---
+++ src/parser/expressions/stackData.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 06/08/2024.
+//
+
+#ifndef STACKDATA_H
+#define STACKDATA_H
+
+#endif //STACKDATA_H
