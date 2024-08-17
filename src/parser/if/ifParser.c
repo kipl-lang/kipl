4ffd7cfce96a70eeefbe0ff0424936148a9f82dd
Kerem Mert <kerem37mert@yahoo.com>
2024-08-17 18:11:56 +0300

    ifParser created

---
+++ src/parser/if/ifParser.c
@@ -0,0 +1,5 @@
+//
+// Created by Kerem on 17/08/2024.
+//
+
+#include "ifParser.h"
---
+++ src/parser/if/ifParser.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 17/08/2024.
+//
+
+#ifndef IFPARSER_H
+#define IFPARSER_H
+
+#endif //IFPARSER_H
