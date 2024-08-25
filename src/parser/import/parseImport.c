da089e7056d4d1673d3ff5c7c294878bfe98dfe0
Kerem Mert <kerem37mert@yahoo.com>
2024-08-25 11:22:54 +0300

    parseImport created

---
+++ src/parser/import/parseImport.c
@@ -0,0 +1,5 @@
+//
+// Created by Kerem on 25/08/2024.
+//
+
+#include "parseImport.h"
---
+++ src/parser/import/parseImport.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 25/08/2024.
+//
+
+#ifndef PARSEIMPORT_H
+#define PARSEIMPORT_H
+
+#endif //PARSEIMPORT_H
