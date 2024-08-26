5955ba1afb930065acc0cfdd3284742f3dd2f989
Kerem Mert <kerem37mert@yahoo.com>
2024-08-26 16:00:19 +0300

    variable.h updated

--- src/parser/variable/variable.h
+++ src/parser/variable/variable.h
@@ -23,6 +23,7 @@ typedef struct _i8_variable {
     char* name;
     int8_t value;
     int8_t* address;
+    bool isMutable;
     struct _i8_variable* next;
 } I8Variable;
 
@@ -30,6 +31,7 @@ typedef struct _i16_variable {
     char* name;
     int16_t value;
     int16_t* address;
+    bool isMutable;
     struct _i16_variable* next;
 } I16Variable;
 
@@ -37,6 +39,7 @@ typedef struct _i32_variable {
     char* name;
     int32_t value;
     int32_t* address;
+    bool isMutable;
     struct _i32_variable* next;
 } I32Variable;
 
@@ -44,6 +47,7 @@ typedef struct _i64_variable {
     char* name;
     int64_t value;
     int64_t* address;
+    bool isMutable;
     struct _i64_variable* next;
 } I64Variable;
 
@@ -51,6 +55,7 @@ typedef struct _i128_variable {
     char* name;
     __int128_t value;
     __int128_t* address;
+    bool isMutable;
     struct _i128_variable* next;
 } I128Variable;
 
@@ -58,6 +63,7 @@ typedef struct _u8_variable {
     char* name;
     uint8_t value;
     uint8_t* address;
+    bool isMutable;
     struct _u8_variable* next;
 } U8Variable;
 
@@ -65,6 +71,7 @@ typedef struct _u16_variable {
     char* name;
     uint16_t value;
     uint16_t* address;
+    bool isMutable;
     struct _u16_variable* next;
 } U16Variable;
 
@@ -72,6 +79,7 @@ typedef struct _u32_variable {
     char* name;
     uint32_t value;
     uint32_t* address;
+    bool isMutable;
     struct _u32_variable* next;
 } U32Variable;
 
@@ -79,6 +87,7 @@ typedef struct _u64_variable {
     char* name;
     uint64_t value;
     uint64_t* address;
+    bool isMutable;
     struct _u64_variable* next;
 } U64Variable;
 
@@ -86,6 +95,7 @@ typedef struct _u128_variable {
     char* name;
     __uint128_t value;
     __uint128_t* address;
+    bool isMutable;
     struct _u128_variable* next;
 } U128Variable;
 
@@ -93,6 +103,7 @@ typedef struct _f32_variable {
     char* name;
     float value;
     float* address;
+    bool isMutable;
     struct _f32_variable* next;
 } F32Variable;
 
@@ -100,6 +111,7 @@ typedef struct _f64_variable {
     char* name;
     double value;
     double* address;
+    bool isMutable;
     struct _f64_variable* next;
 } F64Variable;
 
@@ -107,6 +119,7 @@ typedef struct _bool_variable {
     char* name;
     bool value;
     bool* address;
+    bool isMutable;
     struct _bool_variable* next;
 } BoolVariable;
 
@@ -114,6 +127,7 @@ typedef struct _string_variable {
     char* name;
     char* value;
     char** address;
+    bool isMutable;
     struct _string_variable* next;
 } StringVariable;
 
