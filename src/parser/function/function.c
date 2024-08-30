bf39622337a36c3d202c5e2078710b1353986748
Kerem Mert <kerem37mert@yahoo.com>
2024-08-30 10:51:38 +0300

    function created

---
+++ src/parser/function/function.c
@@ -0,0 +1,5 @@
+//
+// Created by Kerem on 30/08/2024.
+//
+
+#include "function.h"
---
+++ src/parser/function/function.h
@@ -0,0 +1,12 @@
+//
+// Created by Kerem on 30/08/2024.
+//
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+typedef struct _function {
+
+} Function;
+
+#endif //FUNCTION_H
