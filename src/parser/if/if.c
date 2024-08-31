875b35d90a9cb2a1c5a1b5f84256a82b2260269f
Kerem Mert <kerem37mert@yahoo.com>
2024-08-31 18:55:50 +0300

    if updated

--- src/parser/if/if.c
+++ src/parser/if/if.c
@@ -5,7 +5,15 @@
 #include "if.h"
 #include <stdlib.h>
 
+#include "../global.h"
+
 void createIf(unsigned int bracketsNumber) {
     If* newIf = (If*) malloc(sizeof(If));
-    newIf->parentIf =
+    newIf->parentIf = currentIf;
+    newIf->lastBracketsNumber = bracketsNumber;
+    currentIf = newIf;
+}
+
+void freeIf(If* _if) {
+    free(_if);
 }
