435c8cd66f9e8deb1150248e69711f6ca851a489
Kerem Mert <kerem37mert@yahoo.com>
2024-08-31 18:46:05 +0300

    if.h updated

--- src/parser/if/if.c
+++ src/parser/if/if.c
@@ -3,3 +3,5 @@
 //
 
 #include "if.h"
+
+#include <stdlib.h>
--- src/parser/if/if.h
+++ src/parser/if/if.h
@@ -5,6 +5,15 @@
 #ifndef IF_H
 #define IF_H
 
+#include "../../token/token.h"
 
+typedef struct _if {
+    struct _if* parentIf;
+    unsigned int lastBracketsNumber;
+} If;
+
+void createIf(unsigned int bracketsNumber);
+
+void freeIf(If* _if);
 
 #endif //IF_H
