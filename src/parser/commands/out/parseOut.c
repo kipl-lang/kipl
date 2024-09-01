76faf93659a8f3ea590751f89b5765132116ecf1
Kerem Mert <kerem37mert@yahoo.com>
2024-09-01 18:35:16 +0300

    commands/parseOut added

---
+++ src/parser/commands/out/parseOut.c
@@ -0,0 +1,5 @@
+//
+// Created by Kerem on 01/09/2024.
+//
+
+#include "parseOut.h"
---
+++ src/parser/commands/out/parseOut.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 01/09/2024.
+//
+
+#ifndef PARSEOUT_H
+#define PARSEOUT_H
+
+#endif //PARSEOUT_H
