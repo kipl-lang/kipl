5dabf8302fa55406bd3e56b7e8ed08bf5bc37160
Kerem Mert <kerem37mert@yahoo.com>
2024-09-04 14:18:16 +0300

    switch added

--- src/parser/switch/parseSwitch.c
+++ src/parser/switch/parseSwitch.c
@@ -5,10 +5,18 @@
 #include "parseSwitch.h"
 
 #include "../global.h"
+#include "../error/error.h"
 #include "../expressions/parseExpressions.h"
 
 void parseSwitch() {
     unsigned int lastOpenCurlyBracket = openCurlyBracket;
     currentToken = currentToken->next;
     Data* data = parseExpressions();
+
+    if(currentToken->type == TOKEN_BRACKET_CURLY_LEFT) {
+        openCurlyBracket++;
+        currentToken = currentToken->next;
+    } else {
+        showError(ERROR_SYNTAX, " expected '{' after switch clause");
+    }
 }
---
+++ src/parser/switch/switch.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 04/09/2024.
+//
+
+#ifndef SWITCH_H
+#define SWITCH_H
+
+#endif //SWITCH_H
