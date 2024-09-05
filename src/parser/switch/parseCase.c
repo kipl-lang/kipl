bf397192a6373461c3079e8474b4dcffe2534d8b
Kerem Mert <kerem37mert@yahoo.com>
2024-09-05 14:50:36 +0300

    parseCase created

---
+++ src/parser/switch/parseCase.c
@@ -0,0 +1,5 @@
+//
+// Created by Kerem on 05/09/2024.
+//
+
+#include "parseCase.h"
---
+++ src/parser/switch/parseCase.h
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 05/09/2024.
+//
+
+#ifndef PARSECASE_H
+#define PARSECASE_H
+
+#endif //PARSECASE_H
