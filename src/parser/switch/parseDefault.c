d14e03ac964239554ce029a5a486be13919483d4
Kerem Mert <kerem37mert@yahoo.com>
2024-09-06 19:12:41 +0300

    parseDefault added

---
+++ src/parser/switch/parseDefault.c
@@ -0,0 +1,5 @@
+//
+// Created by Kerem on 06/09/2024.
+//
+
+#include "parseDefault.h"
---
+++ src/parser/switch/parseDefault.h
@@ -0,0 +1,10 @@
+//
+// Created by Kerem on 06/09/2024.
+//
+
+#ifndef PARSEDEFAULT_H
+#define PARSEDEFAULT_H
+
+void parseDefault();
+
+#endif //PARSEDEFAULT_H
