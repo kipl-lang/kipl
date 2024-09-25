e896945387c4b88b8326a5374ffee5770a334794
Kerem Mert <kerem37mert@yahoo.com>
2024-09-25 11:04:49 +0300

    DataLinkedList struct added

--- src/parser/helpers/dataLinkedList/dataLinkedList.h
+++ src/parser/helpers/dataLinkedList/dataLinkedList.h
@@ -5,4 +5,11 @@
 #ifndef DATALINKEDLIST_H
 #define DATALINKEDLIST_H
 
+#include "../../expressions/data.h"
+
+typedef struct _data_linked_list {
+    Data* data;
+    struct _data_linked_list* next;
+} DataLinkedList;
+
 #endif //DATALINKEDLIST_H
