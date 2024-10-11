224ebed0b36238dbfcd4b96088d5963e4db7329c
Kerem Mert <kerem37mert@yahoo.com>
2024-10-11 10:51:17 +0300

    outln created

---
+++ src/builtin/outln/outln.c
@@ -0,0 +1,10 @@
+//
+// Created by Kerem on 11/10/2024.
+//
+
+#include "outln.h"
+#include <stdio.h>
+
+void outln() {
+    printf("merhaba\n");
+}
---
+++ src/builtin/outln/outln.h
@@ -0,0 +1,10 @@
+//
+// Created by Kerem on 11/10/2024.
+//
+
+#ifndef OUTLN_H
+#define OUTLN_H
+
+void outln();
+
+#endif //OUTLN_H
